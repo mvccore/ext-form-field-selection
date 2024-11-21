b8b3940097bad62c11dbe7d7778478b4206aa4b3
Tom Flídr <tomflidr@gmail.com>
2024-11-21 16:01:00 +0100

    release v5.3

--- src/MvcCore/Ext/Forms/Fields/Select.php
+++ src/MvcCore/Ext/Forms/Fields/Select.php
@@ -44,7 +44,7 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.9';
+	const VERSION = '5.3.0';
 
 	/**
 	 * Possible value: `select`, not used in HTML code for this field.
